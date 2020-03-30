79cc066450aa2414137a4d6e3424930fdb566528
Helionunes <helio.nunes.rj@hotmail.com>
2020-03-30 00:38:32 -0300

    Update OngController.js

--- backend/src/controllers/OngController.js
+++ backend/src/controllers/OngController.js
@@ -9,7 +9,7 @@ module.exports = {
   },
 
   async create(req, res) {
-    const { name, email, whatsapp, city, uf } = req.body;
+    const { name, email, whatsapp, city, bairro, uf } = req.body;
 
     const id = generateUniqueId();
 
@@ -19,6 +19,7 @@ module.exports = {
       email,
       whatsapp,
       city,
+      bairro,
       uf
     });
 
